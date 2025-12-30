3fe77f8b5c93fa30a05729d87bce91adc9df787b
Tino Gustin <gustintino@gmail.com>
2025-12-31 00:28:52 +0100

    We have a working input detection and char matching

--- monkee-forms/Form1.Designer.cs
+++ monkee-forms/Form1.Designer.cs
@@ -28,59 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.rootPanel = new System.Windows.Forms.TableLayoutPanel();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.rootPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // rootPanel
+            // 
+            this.rootPanel.AutoSize = true;
+            this.rootPanel.ColumnCount = 2;
+            this.rootPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.rootPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.rootPanel.Controls.Add(this.button1, 1, 1);
+            this.rootPanel.Controls.Add(this.mainPanel, 0, 0);
+            this.rootPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rootPanel.Location = new System.Drawing.Point(0, 0);
+            this.rootPanel.Name = "rootPanel";
+            this.rootPanel.RowCount = 2;
+            this.rootPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.rootPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.rootPanel.Size = new System.Drawing.Size(945, 518);
+            this.rootPanel.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(636, 299);
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(748, 461);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 43);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate New Text";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.StartNewRound);
             // 
-            // textBox1
+            // mainPanel
             // 
-            this.textBox1.Location = new System.Drawing.Point(353, 214);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(206, 65);
-            this.textBox1.TabIndex = 1;
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(316, 86);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(273, 61);
-            this.richTextBox1.TabIndex = 2;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(3, 3);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(739, 452);
+            this.mainPanel.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(945, 518);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rootPanel);
             this.Name = "Form1";
             this.Text = "test";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.rootPanel.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.TableLayoutPanel rootPanel;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel mainPanel;
     }
 }
 
--- monkee-forms/Form1.cs
+++ monkee-forms/Form1.cs
@@ -12,31 +12,117 @@ using System.Windows.Forms;
 namespace monkee_forms
 {
     public partial class Form1 : Form
-    {
+    { 
         private readonly TypeRacerApi _api;
+
+        // ELEMENTS
+        private RichTextBox _referenceBox; 
+        private TextBox _inputBox; // for actually capturing the input from user 
+
+        private int _index = 0;
+        private string _referenceText = "";
+
         public Form1(TypeRacerApi api)
         {
             InitializeComponent();
+
             _api = api;
+            InitialSetup();
+            HandleEvents();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void InitialSetup()
+        { 
+            _referenceBox = new RichTextBox();
+            mainPanel.Controls.Add(_referenceBox);
+
+            _inputBox = new TextBox();
+            Controls.Add(_inputBox);
+
+
+            _referenceBox.Dock = DockStyle.Fill;
+            _referenceBox.ReadOnly = true;
+            //_referenceBox.BorderStyle = BorderStyle.None;
+            _referenceBox.ScrollBars = RichTextBoxScrollBars.None;
+            _referenceBox.TabStop = false;
+            _referenceBox.HideSelection = true;
+            _referenceBox.Font = new Font("Microsoft Sans Serif", 20f);
+
+            _inputBox.Size = new Size(1, 1);
+            _inputBox.Location = new Point(-200, -200);
+
+
+
+        }
+
+
+        // maybe i'll wanna do it some other way? dunno yet
+        // if i'm doing it like this, i'll need to add any other elements as well
+        // the inputbox is not on screen, and is therefore not clickable, so focus should be manual
+        private void HandleEvents()
+        {
+            this.Click += (_, __) => _inputBox.Focus();
+            this.Shown += (_, __) => _inputBox.Focus();
+            rootPanel.Click += (_, __) => _inputBox.Focus();
+            mainPanel.Click += (_, __) => _inputBox.Focus();
+
+            _inputBox.KeyPress += InputBox_KeyPress;
+            _inputBox.KeyDown += InputBox_KeyDown;
+
+            // is this even needed?
+            //button1.Click += (_, __) => _inputBox.Focus();
+        }
+
+        // TODO: handle index out of range
+        private void InputBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsControl(e.KeyChar))
+            {
+                // we Will be handling this in the KeyDown
+                e.Handled = true;
+                return;
+            }
+
+            HighlightClear();
+
+            _referenceBox.Select(_index, 1);
+            _referenceBox.SelectionColor = (e.KeyChar == _referenceText[_index]) ? Color.Green : Color.Red;
+            _referenceBox.DeselectAll();
+
+            _index++;
+            HighlightChar();
+            e.Handled = true; 
+        }
+
+        private void HighlightChar()
+        {
+            _referenceBox.Select(_index, 1);
+            _referenceBox.SelectionBackColor = Color.LightGray;
+            _referenceBox.DeselectAll(); 
+        }
+
+        private void HighlightClear()
+        {
+            _referenceBox.Select(_index, 1);
+            _referenceBox.SelectionBackColor = mainPanel.BackColor;
+            _referenceBox.DeselectAll(); 
+        }
+
+        private void InputBox_KeyDown(object sender, KeyEventArgs e)
         {
 
         }
 
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        // called when clicking on the button
+        private async void StartNewRound(object sender, EventArgs e)
         {
-            
+            var httpResponse = await _api.GetTextAsync();
+            _referenceText = JsonSerializer.Deserialize<TextResponse>(httpResponse).data.text;
 
-        }
-
-        private async void button1_Click(object sender, EventArgs e)
-        {
-            var response = await _api.GetTextAsync();
-            var result = JsonSerializer.Deserialize<TextResponse>(response).data.text;
-
-            richTextBox1.Text = result;
+            _referenceBox.Text = _referenceText;
+            _index = 0;
+            HighlightChar();
+            _inputBox.Focus();
         }
     }
 }
